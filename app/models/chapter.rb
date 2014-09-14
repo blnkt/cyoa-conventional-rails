dcdf264eabf96c38c8559a4d21ad3e370a9a4bc8
Blake Mason <blnktd@gmail.com>
2014-09-13 21:44:29 -0700

    add choice/chapter to a chapter only if the choice's prompt is not nil

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,4 +2,8 @@ class Chapter < ActiveRecord::Base
   has_and_belongs_to_many :adventures
   has_one :parent, class_name: 'Chapter', foreign_key: 'parent_id'
   validates :prompt, presence: true
+
+  def add_choice prompt
+  	Chapter.create(prompt: prompt, parent_id: self.id) unless prompt.nil?
+  end
 end 
--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -6,4 +6,19 @@ describe Chapter do
 	it { should have_one  :parent }
 	it { should validate_presence_of :prompt }
 
+	describe '#add_choice' do
+	  it "add a choice to a chapter" do
+	  	parent_chapter = Chapter.create(prompt: "it was a dark and stormy night")
+	    parent_chapter.add_choice("sex")
+	    expect(Chapter.find_by(parent_id: parent_chapter.id).prompt).to eq("sex")
+	  end
+
+	  it "add a choice to a chapter unless the prompt is nil" do
+	  	new_chapter = Chapter.create(prompt: "it was a dark and stormy night")
+	    new_chapter.add_choice("")
+	    expect(Chapter.find_by(parent_id: new_chapter.id)).to eq(nil)
+	  end
+
+	end
+
 end
