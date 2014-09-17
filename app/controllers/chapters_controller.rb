4453dd0d295a00bcf6a55acea46775658aa37a84
Blake Mason <blnktd@gmail.com>
2014-09-17 10:22:11 -0700

    add views and controller actions to add a choice to a chapter: there is a bug that is displaying the array of chapters on the page though

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -6,11 +6,25 @@ class ChaptersController < ApplicationController
     @chapter = Chapter.new()
   end
 
+  def create
+    @adventure = Adventure.find(params[:adventure_id])
+    @chapter = Chapter.find(@adventure.chapters.last.id)
+    prompt = chapter_params[:prompt]
+    unless prompt == ""
+      @chapter.add_choice(prompt)
+      flash[:notice] = "new branch added to #{@adventure.user.name}'s adventure."
+      redirect_to adventure_chapter_path(@adventure, @chapter)
+    else
+      render 'show'
+    end
+  end
+
   def show
     @adventure = Adventure.find(params[:adventure_id])
     @chapter = Chapter.find(params[:id])
     @adventure.add_chapter(@chapter)
     @choices = @chapter.choices
+    @parent = Chapter.find(@chapter.parent_id)
   end
 
   private
